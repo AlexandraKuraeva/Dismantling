2bb9d959b5028f5ac005de73afd7e2b68682b063
AlexandraKuraeva <alexa.kurav@yandex.ru>
2023-04-19 17:40:21 +0400

    Поменяла цвет и эффекты кнопкам

--- src/assets/reviews.js
+++ src/assets/reviews.js
@@ -1,23 +1,23 @@
-const comments = [
+const reviews = [
   {
     id: 1,
     avatar:
       'https://avatars.dzeninfra.ru/get-zen-logos/1526540/pub_620e3cd45e573b68ee048ce2_6211945c0cef08624308b592/xxh',
-    author: 'Антон',
+    name: 'Антон',
     text: 'Я очень доволен работой. Они были очень организованными и профессиональными. Работа была выполнена быстро и без проблем. Я бы порекомендовал эту компанию всем, кто ищет надежного партнера для демонтажа зданий',
   },
   {
     id: 2,
     avatar: 'https://cf-media.press.discovery.com/cache/ugc/photos/33638_002.b68e6ad33272.JPG',
-    author: 'Александр',
+    name: 'Александр',
     text: 'Работники были вежливы и внимательны к нашим потребностям. Работа была выполнена быстро и без проблем. Я бы порекомендовал эту компанию всем, кто ищет надежного партнера для демонтажа зданий.',
   },
   {
     id: 0,
     avatar:
       'https://12dz.com/sites/default/files/styles/wery-mobi/public/avse_f/ava5/hamish-albaniya-shkoder-muzhchina-41-god-118921.jpg?itok=4BIFEJzI',
-    author: 'Андрей',
+    name: 'Андрей',
     text: 'Компания  выполнила работу на высшем уровне. Работники были профессиональными и внимательными к деталям. Все было сделано в срок и без задержек. Рекомендую!',
   },
 ];
-export default comments;
+export default reviews;
--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -3,7 +3,7 @@ import Slider from 'react-slick';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
 import '../scss/slider.scss';
-import comments from '../assets/comments';
+import reviews from '../assets/reviews';
 
 const SliderComments = () => {
   const settings = {
@@ -20,16 +20,16 @@ const SliderComments = () => {
     <div className="slider__inner">
       <div className="container">
         <Slider {...settings}>
-          {comments.map((comment) => (
-            <div style={{ width: 100 }} key={comment.id} className="comment">
+          {reviews.map((item) => (
+            <div style={{ width: 100 }} key={item.id} className="comment">
               <div className="comment__inner">
                 <div className="comment__wrap-img-title">
                   <div className="comment__img">
-                    <img src={comment.avatar} alt="аватарка" />
+                    <img src={item.avatar} alt="аватарка" />
                   </div>
-                  <h3>{comment.author}</h3>
+                  <h3>{item.name}</h3>
                 </div>
-                <p>{comment.text}</p>
+                <p>{item.text}</p>
               </div>
             </div>
           ))}
